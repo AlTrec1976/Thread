68fe42f935b300935c9fd53da872486a979b7859
ALEX-PC\Alex <alex@altrec.ru>
2024-06-15 09:21:58 +0500

    Вывод из потока

---
+++ Tread/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Concurrent;
+using Tread;
+
+namespace ConsoleApp12;
+
+public class Program
+{
+       static ThreadPoolWorker threadPoolWorker = new ThreadPoolWorker(Exampel);
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Method Main Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+        /*
+        Console.WriteLine("Please press any key...");
+        Console.ReadKey();
+        */
+
+        var rand = new Random();
+        threadPoolWorker.Start(rand.Next(1, 10));
+
+
+        for (int i = 0; i < 40; i++)
+        {
+            Console.Write('-');
+            
+            Thread.Sleep(100);
+        }
+
+        Console.WriteLine(threadPoolWorker.Wait());
+
+        /*
+        Report();
+        ThreadPool.QueueUserWorkItem(new WaitCallback(Example1));
+        Report();
+        ThreadPool.QueueUserWorkItem(new WaitCallback(Example2));
+        Report();
+
+        Console.ReadKey();
+        Report();
+    */
+    }
+
+    public static void Print()
+    {
+        
+    }
+
+    public static int Exampel(object num)
+    {
+        var rand = new Random();
+        for (int i = 0; i < 40; i++)
+        {
+            //Console.Write(workThreads + " ");
+            Thread.Sleep(100);
+            Console.WriteLine((int)num);
+            
+        }
+        //Console.WriteLine(threadPoolWorker.Wait());
+
+        return (int)num;
+    }
+
+    private static void WriteChar(object obj)
+    {
+        char item = (char)obj;
+
+        for (int i = 0; i < 220; i++)
+        {
+            Console.Write(item);
+            Thread.Sleep(500);
+        }
+    }
+
+    private static void Example1(object state)
+    {
+        Console.WriteLine($"Method Example1 begean work in Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(2000);
+        Console.WriteLine($"Method Example1 end work in Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    private static void Example2(object state)
+    {
+        Console.WriteLine($"Method Example2 begean work in Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+        Thread.Sleep(1000);
+        Console.WriteLine($"Method Example2 end work in Thread ID - {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    private static void Report()
+    {
+        ThreadPool.GetMaxThreads(out int maxWorkThreads, out int maxPortThreads);
+        ThreadPool.GetAvailableThreads(out int workThreads, out int portThreads);
+
+        Console.WriteLine($"work thread {workThreads} from {maxWorkThreads}");
+        Console.WriteLine(new string('-', 80));
+    }
+
+    private static void PrintChar(object arg)
+    {
+        Console.WriteLine($"Current thread Id: ${Thread.CurrentThread.ManagedThreadId}");
+        char item = (char)arg;
+        for (int i = 0; i < 800; i++)
+        {
+            Console.Write(item);
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Tread/ThreadPoolWorker.cs
@@ -0,0 +1,59 @@
+﻿namespace Tread;
+
+public class ThreadPoolWorker
+{
+    private int f;
+    //private readonly Action<object> _action;
+    private readonly Func<object, int> _func;
+    
+    public ThreadPoolWorker(Func<object, int> funk)
+    {
+        //_action = action ?? throw new ArgumentNullException();
+        _func = funk ?? throw new ArgumentNullException();
+    }
+
+    public bool Success { get; private set; } = false;
+    public bool IsComplete { get; private set; } = false;
+
+    public void Start(object num)
+    {
+        ThreadPool.QueueUserWorkItem(new WaitCallback(Execute), OutPut((int)num));
+    }
+
+    public string Wait()
+    {
+        Console.WriteLine($"Wait start in {Thread.CurrentThread.ManagedThreadId}");
+        while (!IsComplete)
+        {
+            Thread.Sleep(150);
+        }
+        return $"Wait is finisched {OutPut(f)}";
+    }
+
+    private void Execute(object num)
+    {
+        try
+        {
+            Console.WriteLine($"Execute start in {Thread.CurrentThread.ManagedThreadId}");
+            //_action.Invoke(state);
+            f = _func.Invoke(num);
+            
+            Success = true;
+        }
+        catch (Exception ex)
+        {
+            Success = false;
+        }
+        finally
+        {
+            IsComplete = true;
+        }
+        //return (int)num + rand.Next(100, 1000);
+    }
+
+    private int OutPut(int num)
+    {
+        var rand = new Random();
+        return num + rand.Next(10, 30);
+    }
+}
